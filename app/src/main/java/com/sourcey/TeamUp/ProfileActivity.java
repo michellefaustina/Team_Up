b41d74ec01e8c1def286ea47d23d9fba355f3348
Michellefaustina <michelle.delapan@gmail.com>
2015-11-04 13:01:12 -0800

    add profile_activity java and xml

---
+++ app/src/main/java/com/sourcey/TeamUp/ProfileActivity.java
@@ -0,0 +1,39 @@
+package com.sourcey.TeamUp;
+
+import android.content.Intent;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import butterknife.InjectView;
+        import butterknife.ButterKnife;
+
+//Created by Michelle on 10/30/15.
+
+public class ProfileActivity extends AppCompatActivity {
+    // Open when called
+    public static final String TAG = "ProfileActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        ImageButton backBtn = (ImageButton)findViewById(R.id.backBtn);
+
+        // Back
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
